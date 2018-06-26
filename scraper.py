2151837d84181a3ec6446cd3a425a63c7d7a7c65
marcusfgardiner <marcusfgardiner@gmail.com>
2018-06-26 09:46:38 +0100

    Simple scraping with beautiful soup completed

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #import libraries
 from urllib.request import urlopen
+import re
 from bs4 import BeautifulSoup
 
 # text to find: 'Preston and Lancashire'
@@ -7,3 +8,7 @@ from bs4 import BeautifulSoup
 web_page = 'https://en.wikipedia.org/wiki/PR'
 page = urlopen(web_page)
 print(page)
+soup = BeautifulSoup(page, 'html.parser')
+
+for elem in soup(text=re.compile(r'Preston')):
+    print(elem)
