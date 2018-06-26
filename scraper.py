e0561429bfc807176685c8bdc2eddf10c78e8a22
marcusfgardiner <marcusfgardiner@gmail.com>
2018-06-26 08:52:51 +0100

    Web page object saved, next is to parse it with beautiful soup

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
-import libraries
-import urllib2
+#import libraries
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 # text to find: 'Preston and Lancashire'
 
-web_page = https: // en.wikipedia.org/wiki/PR
-page = urllib2.urlopen(web_page)
+web_page = 'https://en.wikipedia.org/wiki/PR'
+page = urlopen(web_page)
+print(page)
