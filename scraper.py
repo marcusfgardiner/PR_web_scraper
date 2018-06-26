d5e135643c8d275369e16fdadb7e945bfa66c8b7
marcusfgardiner <marcusfgardiner@gmail.com>
2018-06-26 08:45:37 +0100

    Web page object and open

--- scraper.py
+++ scraper.py
@@ -1,3 +1,8 @@
 import libraries
 import urllib2
 from bs4 import BeautifulSoup
+
+# text to find: 'Preston and Lancashire'
+
+web_page = https: // en.wikipedia.org/wiki/PR
+page = urllib2.urlopen(web_page)
